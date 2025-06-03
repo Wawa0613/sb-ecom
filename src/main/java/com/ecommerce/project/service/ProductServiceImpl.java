5684389c5e0da855c3825930c136b6c9cfca48d7
Wawa0613 <xianglee07269@gmail.com>
2025-06-03 15:52:04 -0700

    Making Use of FileService and application.properties

--- src/main/java/com/ecommerce/project/service/ProductServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/ProductServiceImpl.java
@@ -9,6 +9,7 @@ import com.ecommerce.project.repositories.CategoryRepository;
 import com.ecommerce.project.repositories.ProductRespository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -29,6 +30,11 @@ public class ProductServiceImpl implements ProductService{
    private CategoryRepository categoryRepository;
     @Autowired
     private ModelMapper modelMapper;
+    @Autowired
+    private FileService fileService;
+
+    @Value("${project.image}")
+    private String path;
     @Override
     public ProductDTO addProduct(Long categoryId, ProductDTO productDTO) {
         Category category=categoryRepository.findById(categoryId)
@@ -110,26 +116,13 @@ public class ProductServiceImpl implements ProductService{
     public ProductDTO updateProductImage(Long productId, MultipartFile image) throws IOException {
         Product productFromDb=productRespository.findById(productId)
                 .orElseThrow(()->new ResourceNotFoundException("Product","productId",productId));
-        String path="images";
-        String fileName=uploadImage(path, image);
+
+        String fileName=fileService.uploadImage(path, image);
         productFromDb.setImage(fileName);
         Product updatedProduct=productRespository.save(productFromDb);
 
         return modelMapper.map(updatedProduct, ProductDTO.class);
     }
 
-    private String uploadImage(String path, MultipartFile file) throws IOException {
 
-        String originalFileName=file.getOriginalFilename();
-        String randomId= UUID.randomUUID().toString();
-        String fileName=randomId.concat(originalFileName.substring(originalFileName.lastIndexOf(".")));
-        String filePath=path+ File.separator+fileName;
-
-        File folder=new File(path);
-        if(!folder.exists()){
-            folder.mkdirs();
-        }
-        Files.copy(file.getInputStream(), Paths.get(filePath));
-        return fileName;
-    }
 }
