acf37d9d0e423872b35d0ded1b886d3031beffe1
Wawa0613 <xianglee07269@gmail.com>
2025-06-03 20:59:49 -0700

    Setting up uniqueConstraints attribute in userTable

--- src/main/java/com/ecommerce/project/model/User.java
+++ src/main/java/com/ecommerce/project/model/User.java
@@ -15,22 +15,34 @@ import java.util.Set;
 @Entity
 @Data
 @NoArgsConstructor
-@Table(name="Users")
+@Table(name="Users",
+        uniqueConstraints = {
+        @UniqueConstraint(columnNames="userName"),
+        @UniqueConstraint(columnNames = "email")
+        })//@Table 注解中的 uniqueConstraints
+//用于在数据库表中声明“组合唯一约束”或“字段唯一性约束”。
+//
+//比如可以防止两个用户拥有相同的 userName 或 email。告诉数据库： userName 和 email 这两个字段必须是唯一的；
+//自动生成 SQL： JPA 在建表时，会在 Users 表中添加两个 UNIQUE 约束；
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="user_id")
     private Long userId;
     @NotBlank
     @Size(max=20)
+    @Column(name="userName")
     private String userName;
 
     @NotBlank
     @Size(max=50)
     @Email
+    @Column(name="email")
     private String email;
 
     @NotBlank
     @Size(max=120)
+    @Column(name="password")
     private String password;
 
     public User(String userName, String email, String password) {
@@ -43,7 +55,7 @@ public class User {
     @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE},
                fetch = FetchType.EAGER)
     @JoinTable(name="user_role",
-               joinColumns = @JoinColumn(name="role_id"),
+               joinColumns = @JoinColumn(name="user_id"),
                inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles=new HashSet<>();
 }
